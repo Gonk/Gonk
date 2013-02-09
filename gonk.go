ea92b45a337a6aaeeaed65089af00c6c92e86b9e
Cameron Currie <me@cameroncurrie.net>
2013-02-09 17:31:03 -0600

    Implement JavaScript module loading

--- gonk.go
+++ gonk.go
@@ -6,8 +6,12 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/signal"
+	//"os/signal"
+	"io/ioutil"
+	"regexp"
+	"strings"
 
+	"github.com/cwc/go-v8"
 	irc "github.com/fluffle/goirc/client"
 )
 
@@ -17,19 +21,69 @@ func printUsageAndExit() {
 	os.Exit(1)
 }
 
+func loadModules(conn *irc.Conn) (modules []Module) {
+	// Load each module in the modules directory
+	scripts, err := ioutil.ReadDir("modules")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fileInfo := range scripts {
+		if !fileInfo.IsDir() {
+			if !strings.HasSuffix(fileInfo.Name(), "js") {
+				continue
+			}
+
+			file, err := os.Open("modules/" + fileInfo.Name())
+			if err != nil {
+				log.Println("Error loading module:", err)
+				continue
+			}
+
+			defer file.Close()
+
+			script, err := ioutil.ReadAll(file)
+			if err != nil {
+				log.Println("Error loading module:", err)
+				continue
+			}
+
+			v8ctx := v8.NewContext()
+			module := newModule(fileInfo.Name(), conn, v8ctx)
+
+			_, err = module.Init(string(script))
+
+			if err != nil {
+				log.Println("Error loading module:", err)
+			}
+
+			log.Printf("Loaded module %s", fileInfo.Name())
+			modules = append(modules, module)
+		}
+	}
+
+	return
+}
+
+func newModule(name string, client *irc.Conn, context *v8.V8Context) Module {
+	return Module{name, client, context, make(map[*regexp.Regexp]v8.V8Function), make(map[*regexp.Regexp]v8.V8Function)}
+}
+
 func main() {
-	quitting := make(chan bool)
+	//quitting := make(chan bool)
 	disconnecting := make(chan bool)
 
-	// Set up ^C handler
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-	go func() {
-		// Quit on ^C
-		<-interrupt
-		quitting <- true
-	}()
+	/*/ Set up ^C handler
+		interrupt := make(chan os.Signal, 1)
+		signal.Notify(interrupt, os.Interrupt)
+		go func() {
+			// Quit on ^C
+			<-interrupt
+			quitting <- true
+		}()
+	    //*/
 
+	// Parse flags
 	server := flag.String("server", "", "Hostname and/or port (e.g. 'localhost:6667')")
 	gonkNick := flag.String("nick", "gonk", "Nickname used for the connection")
 	password := flag.String("password", "", "Server password")
@@ -43,6 +97,7 @@ func main() {
 		printUsageAndExit()
 	}
 
+	// Setup IRC client
 	c := irc.SimpleClient(*gonkNick)
 
 	if *ssl {
@@ -52,6 +107,9 @@ func main() {
 	if !*verifyCert {
 		c.SSLConfig = &tls.Config{InsecureSkipVerify: true}
 	}
+
+	// Load modules and connect them to the IRC client
+	modules := loadModules(c)
 
 	c.AddHandler("connected", func(conn *irc.Conn, line *irc.Line) {
 		// Join all specified channels upon connecting
@@ -72,12 +130,23 @@ func main() {
 	c.AddHandler("privmsg", func(conn *irc.Conn, line *irc.Line) {
 		// Determine reply target
 		target := line.Args[0]
-		if target == *gonkNick {
+		if target == conn.Me.Nick {
+			// Reply via PM
 			target = line.Nick
 		}
 
-		// Send reply
-		c.Privmsg(target, "*GONK*")
+		text := strings.Join(line.Args[1:], "")
+
+		go func() {
+			for _, module := range modules {
+				if target == line.Nick || strings.HasPrefix(text, conn.Me.Nick) {
+					// Received a PM or addressed directly in a channel
+					module.Respond(target, text)
+				} else {
+					module.Hear(target, text)
+				}
+			}
+		}()
 	})
 
 	err := c.Connect(*server, *password)
@@ -85,8 +154,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	go func() {
-		<-quitting
+	defer func() {
+		//<-quitting
 
 		c.Quit("*GONK*")
 
---
+++ module.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"regexp"
+	"strings"
+
+	"github.com/cwc/go-v8"
+	irc "github.com/fluffle/goirc/client"
+)
+
+type Module struct {
+	Name    string
+	Client  *irc.Conn
+	Context *v8.V8Context
+
+	respondMatchers map[*regexp.Regexp]v8.V8Function
+	hearMatchers    map[*regexp.Regexp]v8.V8Function
+}
+
+func (m Module) Respond(target string, line string) {
+	// Store the target
+	m.setTarget(target)
+
+	// Activate callback on any matches
+	for regex, fn := range m.respondMatchers {
+		count := m.setMatches(regex, line)
+		if count > 0 {
+			fn.Call(`response`)
+		}
+	}
+}
+
+func (m Module) Hear(target string, line string) {
+	// Store the target
+	m.setTarget(target)
+
+	// Activate callback on any matches
+	for regex, fn := range m.hearMatchers {
+		count := m.setMatches(regex, line)
+		if count > 0 {
+			fn.Call(`response`)
+		}
+	}
+}
+
+func (m Module) setTarget(target string) {
+	m.Context.Eval(`response.target = "` + target + `"`)
+}
+
+func (m Module) setMatches(regex *regexp.Regexp, line string) int {
+	matches := regex.FindStringSubmatch(line)
+	match, _ := json.Marshal(matches)
+
+	m.Context.Eval(`response.match = ` + string(match))
+
+	return len(match)
+}
+
+func (m Module) Init(script string) (ret interface{}, err error) {
+	v8ctx := m.Context
+
+	v8ctx.AddFunc("_console_log", func(args ...interface{}) interface{} {
+		for _, arg := range args {
+			log.Printf("> %s", arg)
+		}
+
+		return ""
+	})
+
+	v8ctx.AddFunc("_robot_respond", func(args ...interface{}) interface{} {
+		regex := args[0].(*regexp.Regexp)
+		fn := args[1].(v8.V8Function)
+
+		m.respondMatchers[regex] = fn
+
+		return ""
+	})
+
+	v8ctx.AddFunc("_robot_hear", func(args ...interface{}) interface{} {
+		regex := args[0].(*regexp.Regexp)
+		fn := args[1].(v8.V8Function)
+
+		m.hearMatchers[regex] = fn
+
+		return ""
+	})
+
+	v8ctx.AddFunc("_msg_send", func(args ...interface{}) interface{} {
+		target := strings.Trim(args[1].(string), `"`)
+		text := strings.Trim(args[0].(string), `"`)
+
+		m.Client.Privmsg(target, text)
+
+		return ""
+	})
+
+	// Set up objects
+	v8ctx.Eval(`console = {
+                "log": function() { return _console_log.apply(null, arguments); }
+    }`)
+
+	v8ctx.Eval(`gonk = {
+                "respond": function() { return _robot_respond.apply(null, arguments); },
+                "hear" : function() { return _robot_hear.apply(null, arguments); }
+    } `)
+
+	v8ctx.Eval(`response = {
+                "send" : function() {
+                    var args = [].slice.call(arguments);
+                    args.push(response.target);
+                    _msg_send.apply(null, args); 
+                }
+    }`)
+
+	v8ctx.Eval(`module = {}`) // Module code is loaded into module.exports
+
+	// Load script
+	ret, err = v8ctx.Eval(script)
+	v8ctx.Eval(`module.exports(gonk)`)
+
+	return
+}
